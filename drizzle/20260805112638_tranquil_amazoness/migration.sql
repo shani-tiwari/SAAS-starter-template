7f7d6239fd31fbcd1d933417b568214108edf6d7
shani-tiwari <shanitiwari2021@gmail.com>
2026-08-05 17:01:44 +0530

    DB connection & table creation

---
+++ drizzle/20260805112638_tranquil_amazoness/migration.sql
@@ -0,0 +1,18 @@
+CREATE TABLE "todo" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "todo_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"user_id" integer,
+	"title" varchar(255) NOT NULL,
+	"completed" boolean DEFAULT false,
+	"created_at" timestamp,
+	"updated_at" timestamp
+);
+--> statement-breakpoint
+CREATE TABLE "user" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "user_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"name" varchar(255) NOT NULL,
+	"email" varchar(255) NOT NULL UNIQUE,
+	"is_subscribed" boolean DEFAULT false,
+	"subscription_ends" timestamp
+);
+--> statement-breakpoint
+ALTER TABLE "todo" ADD CONSTRAINT "todo_user_id_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id");
