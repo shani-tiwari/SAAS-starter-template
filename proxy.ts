b2a600196c2fa5beea365b9a42d5b5513b53fe32
shani-tiwari <shanitiwari2021@gmail.com>
2026-08-07 13:18:24 +0530

    Middleware implementation

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,68 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware, clerkClient } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server';
+// clerkClient - easier to interact with the API
 
-export default clerkMiddleware()
+
+// maybe some changes were there to make route public on clerk level
+const publicRoutes = [
+  '/',
+  'api/webhook/register',
+  'sign-in',
+  'sign-up'
+];
+
+export default clerkMiddleware(async (auth, req) => {
+
+  /**
+   * req.url - full url (https://localhost:3000/sign-in)
+   * req.nextUrl.pathname - pathname of the current url (/sign-in)
+   */
+
+  const { isAuthenticated, redirectToSignIn, userId } = await auth()
+
+  if (!isAuthenticated && !publicRoutes.includes(req.nextUrl.pathname)) return redirectToSignIn();
+
+  try{
+    if(userId) {
+
+      const clerk = await clerkClient();
+      const user = await clerk.users.getUser(userId);
+      const userRole = user.publicMetadata.role as string | undefined;
+
+      // admin user
+      if(userRole ===  "admin" && req.nextUrl.pathname === '/dashboard') {
+      return NextResponse.redirect(new URL('/admin/dashboard', req.url));
+      };
+
+      // non-admin user
+      if(userRole !==  "admin" && req.nextUrl.pathname.startsWith('/admin')) {
+        return NextResponse.redirect(new URL('/dashboard', req.url));
+      };
+
+      // redirect auth user, to not access sign-in/up page
+      if(isAuthenticated && publicRoutes.includes(req.nextUrl.pathname)) {
+        return NextResponse.redirect(new URL('/dashboard', req.url)); 
+      };
+      
+      // redirect auth users trying to use public routes
+      if(publicRoutes.includes(req.nextUrl.pathname)){
+        return NextResponse.redirect(new URL(
+          userRole === 'admin' ? '/admin/dashboard' : '/dashboard'
+        )); 
+      }
+
+    }
+
+    // return NextResponse.next();
+
+  }catch(error){
+    console.error('Error in proxy.ts:', error);
+    return NextResponse.redirect(new URL('/error', req.url));
+  };
+
+
+}, { debug: true });
+
 
 export const config = {
   matcher: [
